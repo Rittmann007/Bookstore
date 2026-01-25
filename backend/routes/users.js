32e253f4daf956a4f8148c4f01cb85d1a0e3247d
Ritom Paul <rittmann007@gmail.com>
2026-01-26 01:42:18 +0530

    Implement book registration endpoint and custom error handling

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const ApiError = require('../utils/Apierror');
+const Book = require('../models/book.model');
+const ApiResponse = require('../utils/Apiresponse');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', async function(req, res, next) {
+
+  try {
+    const {title,author,publishyear} = req.body
+  
+    if (!title || !author || !publishyear) {
+      throw new ApiError(400,"all fields are required")
+    }
+    const existedbook = await Book.findOne({
+      $or: [{title},{author}]
+    })
+  
+    if (existedbook) {
+      throw new ApiError(400,"book already exists")
+    }
+  
+    const createdbook = await Book.create(
+      {
+        title,
+        author,
+        publishyear
+      }
+    )
+  
+    if (!createdbook) {
+      throw new ApiError(400,"something went wrong while registering")
+    }
+  
+    return res.status(201).json(
+      new ApiResponse(200,createdbook,"book registered successfully")
+    )
+  } catch (error) {
+    console.log(error)
+  }
+
+
 });
 
 module.exports = router;
