4d37691fe84e089b348ce1a6ee60e79b7bca9491
Ritom Paul <rittmann007@gmail.com>
2026-01-31 13:20:41 +0530

    configure react and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-red-500'>come on boy</h1>
+    </>
+  )
+}
+
+export default App
